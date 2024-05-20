f6aa6f2f07b5bfad56d430f5f0718bd46407f87c
AZE38 <unfluxensable@gmail.com>
2024-05-20 21:45:01 +0200

    Update 3D_cloud_buffer.js

--- 3D_cloud_buffer.js
+++ 3D_cloud_buffer.js
@@ -925,10 +925,10 @@ let animationFrameId4;
 let darkMatterDensity=0;
 
 var params = {
-    Radius_SImulation: 600,
+    Radius_SImulation: 500,
     scale: 0.0005,
     deltaTime: 0.002, // deltatime simulation in Giga years
-    numStarsNeg: 80000,
+    numStarsNeg: 150000,
     numStarsPos:20000,
     densityNeg: -0.1*Math.pow(10,5), // solar mass / ly^3 (density mass of nega stars)
     TotmassPos: 5*Math.pow(10,11), // ~ milky way solar mass (total mass of the galaxy)
@@ -936,7 +936,7 @@ var params = {
     galactR: 25, // 25 unit x 1/scale = 50 000 ly ~ milky way radius
     elips: 1,
     speedRot: 30, //  rad per billion years (10^9 year)
-    speed_init: 600, // ly/Gy
+    speed_init: 500, // ly/Gy
     colorNeg: '#ff0000',
     colorPos: '#00ff00',
     timestep:100,
